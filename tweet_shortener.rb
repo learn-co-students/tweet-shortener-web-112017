8cfed8050755e9b9dc22a7f08af8531c95cbc5bd
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-07 21:19:27 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def dictionary
+    dictionary = {
+      "hello": "hi",
+      "to": "2",
+      "two": "2",
+      "too": "2",
+      "for": "4",
+      "four": "4",
+      "be": "b",
+      "you": "u",
+      "at": "@",
+      "and": "&"
+    }
+end
+
+def word_substituter(string)
+  a = string.split(" ")
+  a.collect do |word|
+    if dictionary.keys.include?(word)
+      a[word] = dictionary[word]
+    else
+      a[word]
+    end
+  end
+  return a.join(" ")
+end
